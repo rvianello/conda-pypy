04a1a48b280fe5d411a30c902d0c79b2889b0d13
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-05-27 08:28:03 +0200

    first working package

--- pypy/build.sh
+++ pypy/build.sh
@@ -1,7 +1,19 @@
-cd pypy/goal
+pushd ${SRC_DIR}/pypy/goal
 
 CFLAGS=-I${PREFIX}/include \
 LDFLAGS=-L${PREFIX}/lib \
 $PYTHON ../../rpython/bin/rpython -Ojit targetpypystandalone
 
-exit 1
+# any tests I can run from here?
+
+popd
+
+# or from here?
+
+cp ${SRC_DIR}/pypy/goal/pypy-c ${PREFIX}/bin/pypy
+
+cp -r ${SRC_DIR}/site-packages ${PREFIX}
+cp -r ${SRC_DIR}/lib_pypy ${PREFIX}
+cp -r ${SRC_DIR}/lib-python ${PREFIX}
+
+cp -r ${SRC_DIR}/include/* $PREFIX/include
