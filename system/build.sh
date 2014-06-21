f9f8018ef5a14b39c593b9d2d40a3a5e0a788324
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-06-21 13:44:54 +0200

    fixed revisions

--- system/build.sh
+++ system/build.sh
@@ -1,9 +1,11 @@
 mkdir -p $PREFIX/lib $PREFIX/include
 
+pushd $PREFIX/lib
+cp /lib64/libm-2.12.so .
+ln -s libm-2.12.so libm.so.6
+ln -s libm-2.12.so libm.so
+cp -a /usr/lib64/libgfortran.so.* .
+popd
 
-for filename in /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libm-2.17.so /lib64/libm.so.6 /lib64/libm-2.12.so /usr/lib64/libgfortran.so.3.0.0 /usr/lib64/libgfortran.so.3
-do
-  test -f $filename && cp -a $filename $PREFIX/lib
-done
 cp -a /usr/include/math.h  $PREFIX/include
 
