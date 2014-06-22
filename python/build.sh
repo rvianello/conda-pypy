f3f20324cf3cf2e9a1b1192312f498976666ef22
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-06-23 00:21:37 +0200

    install the shared library and fixes to the bytecode compilation

--- python/build.sh
+++ python/build.sh
@@ -16,10 +16,15 @@ cp ${SRC_DIR}/pypy/goal/pypy-c ./python
 ln -s ./python pypy
 popd
 
+# and the shared library
+pushd ${PREFIX}/lib
+cp ${SRC_DIR}/pypy/goal/libpypy-c.so .
+popd
+
 # compile the py modules to bytecode
 ${PREFIX}/opt/python -m compileall ${SRC_DIR}/site-packages
-${PREFIX}/opt/python -m compileall ${SRC_DIR}/site-packages
-${PREFIX}/opt/python -m compileall ${SRC_DIR}/site-packages
+${PREFIX}/opt/python -m compileall ${SRC_DIR}/lib_pypy
+${PREFIX}/opt/python -m compileall ${SRC_DIR}/lib-python
 
 # copy the library code
 cp -r ${SRC_DIR}/site-packages ${PREFIX}
